2f0a8c6222db02f22f3ec1ef69a357c0211f5e79
Yuchen Yang <yangyc@Yuchens-MacBook-Pro.local>
2018-09-16 14:19:03 +0800

    Update make block diagonal comments

--- matlab_data/makeblk.m
+++ matlab_data/makeblk.m
@@ -11,26 +11,36 @@ function Z =makeblk(H,Uk,Zo,index)
 %     (usually k eigenvectors of a Gram or Laplacian matrix)
 % H: n by k matrix; column-orthogonal for nonzero columns
 %     (usually rank deficient)
-% Zo: the original projection by MATLAB default
+% Zo: the original projection from H to U by MATLAB default
+% index: the column number the algorithm starts working on (usually random)
 %====================================================================
 % Output:
-% Z: the desired rotation
+% Z: the desired rotation from H to U
 %====================================================================
+    % all nonzero columns of H
     col_full = find(sum(H~=0));
+    % original U
     U = Uk*Zo;
     k = size(Uk,2);
     trial = 0;
+    % return Zo for full-rank H
     if length(col_full)== k
         Z=Zo;
     else
+        % check whether we can do operations on the certain column
+        % a limit number of trials
         while (trial < min(k,5))
+            % the rows of corresponding nonzero columns
             row_id = find(H(:,col_full(index))>0);
             row_ic = find(H(:,col_full(index))==0);
+            % for a single element, retry 
+            % can be improved later
             if length(row_id)==1
                 index=datasample(1:length(col_full),1);
                 trial=trial+1;
                 continue;
             end
+            % find the approximate rank of U(row_id,:), usually much smaller
             [s,D,v] = svd(U(row_id,:));
             d = diag(D);
             rk = 1;
@@ -40,6 +50,7 @@ function Z =makeblk(H,Uk,Zo,index)
                     break;
                 end
             end
+            % rank==1 means a block diagonal has been found
             if rk==1
                 Z=Zo;
                 break;
@@ -50,33 +61,27 @@ function Z =makeblk(H,Uk,Zo,index)
                     continue;
                 end
                 
-%                 col_else = col_full;
-%                 col_else(index)=[];
-%                 if max(sum(abs(U(row_id,col_else))))>1e-3*length(row_id)
-%                     index=datasample(1:length(col_full),1);
-%                     trial=trial+1;
-%                     continue;
-%                 end
+                % check whether row_id are exactly the combination of
+                % several clusters by looking at its singular values
                 if (abs(d(rk)-1)>1e-3)
                     index=datasample(1:length(col_full),1);
                     trial=trial+1;
                     continue;
                 end
 
-
-                %[~,column_id]=sort(sum(abs(v(:,1:rk)),2),'descend');
+                % if this column is doable, modify the loading matrix v to be sparse
                 column_id = find(sum(abs(v(:,1:rk)),2)>1e-4);
-                %col_keep = column_id(column_id~=col_full(index));
+                % keep rk-1 columns of all zero columns in H
                 col_keep = column_id(ismember(column_id,find(sum(H)==0)));
-                %col_keep = col_keep(1:rk-1);
                 col_keep = datasample(col_keep,rk-1,'Replace',false);
 
-                %vv=sparse([col_keep;col_full(index)],1:rk,ones(1,rk),k,rk,rk);
+                % vv=sparse([col_keep;col_full(index)],1:rk,ones(1,rk),k,rk,rk);
                 vv = zeros(k,rk);
+                % keep the original column
                 vv([col_keep;col_full(index)],:) = v([col_keep;col_full(index)],1:rk);
                 U(row_id,:)=s(:,1:rk)*D(1:rk,1:rk)*vv';
                 U(row_ic,col_keep)=0;
-                
+                % project the modified U back to column Uk
                 [S,~,V]=svd(Uk'*U,0);
                 Z = S*V';
                 break;
