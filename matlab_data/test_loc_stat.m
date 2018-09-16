bafa184e93c4b04463f1ce225e7b26e62f0fd7a4
yangyuchen0340 <yy42@rice.edu>
2018-09-16 21:40:21 +0800

    Modify test local minima stats

--- matlab_data/test_loc_stat.m
+++ matlab_data/test_loc_stat.m
@@ -1,28 +1,22 @@
 % local minimum stats
-function result=apstat(A,maxit,prt)
-% different trial for random matrices and special synethic data
-% function gap=apstat(a,b,prt)
-
-% maxit=100;
-data=zeros(maxit,1);
-parfor sp=1:maxit
-%     [err,N]=ap3(A,1,0);
-%     data(sp)=length(find(max(N)<0.2));
-      [~,~,dis] = kmeans(A,size(A,2),'Start','plus','Replicates',10);
-      data(sp) = norm(dis);
-%       err=ap3(A,0,0);
-%       data(sp)=err(end);
-end
-if prt
-figure;
-plot(data,'*r','Markersize',20);
-axis([0,100,-0.2,6])
-grid on
-set(gca,'Fontsize',20)
-% hold on
-% err=ap1(A,1,0);
-% plot(floor(maxit/2),err(end),'*');
-title('Objectives of K-means++','Fontsize',25);
-end
-result=data;
+function result=test_loc_stat(A,maxit,prt)
+% different initial trials for different local minima
+    data=zeros(maxit,1);
+    parfor sp=1:maxit
+          [~,~,dis] = kmeans(A,size(A,2),'Start','plus','Replicates',10);
+          data(sp) = norm(dis);
+%           KindAP
+%           err=kind_ap(A,0,0);
+%           data(sp)=err(end);
+    end
+    % plot
+    if prt
+        figure;
+        plot(data,'*r','Markersize',15);
+        axis([0,100,-0.2,6])
+        grid on
+        set(gca,'Fontsize',20)
+        title('Objectives of K-means++','Fontsize',25);
+    end
+    result=data;
 end
