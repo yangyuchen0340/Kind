88865d95bb79b3f3b5729afd390dca2049d30220
yangyuchen0340 <yy42@rice.edu>
2020-06-09 23:20:44 -0500

    Remove matlab_version for the brevity of master branch

--- matlab_data/utility/purity.m
+++
@@ -1,8 +0,0 @@
-function rate = purity(predicted_labels,real_labels)
-
-M = crosstab(predicted_labels,real_labels); % you can use also use "confusionmat"
-nc = sum(M,1);
-mc = max(M,[],1);
-rate = sum(mc(nc>0))/sum(nc);
-
-end
